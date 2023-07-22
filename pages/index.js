582656262dbe4e0391fa647392e085c38caeb9f2
razacodespython <srh.zaidi@outlook.com>
2023-07-22 13:45:29 +0200

    paymaster woooorks

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,17 @@ export default function Home() {
 
   const options = {
     method: "POST",
-    headers: {
-      "content-type": "application/json"
-    },
-    body: JSON.stringify(message)
+    // headers: {
+    //   "content-type": "application/json"
+    // },
+    // body: JSON.stringify(message)
   }
 
   const paymaster = async() =>{
     console.log("clicked");
     const response = await fetch("/api/paymaster",options);
     const data = await response.json();
+    console.log(data)
   }
   return (
     <>
