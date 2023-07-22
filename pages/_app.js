70947df67193218d3dbcc5dc053b90e281a4963e
jennyg0 <jen2348@hotmail.com>
2023-07-22 21:48:00 +0200

    Add fonts

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,18 @@ import {
 import { Web3Modal } from "@web3modal/react";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { arbitrum, mainnet, polygon } from "wagmi/chains";
+import { Inter, Press_Start_2P } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const headingFont = Press_Start_2P({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-press-start",
+});
 
 const chains = [arbitrum, mainnet, polygon];
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
@@ -19,13 +31,14 @@ const wagmiConfig = createConfig({
   publicClient,
 });
 const ethereumClient = new EthereumClient(wagmiConfig, chains);
+
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <main className={`${headingFont.variable} ${inter.variable}`}>
       <WagmiConfig config={wagmiConfig}>
         <Component {...pageProps} />
       </WagmiConfig>
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
-    </>
+    </main>
   );
 }
