6033838d6c5ad4fe0f0afbaa5e6b12c440f255f2
jennyg0 <jen2348@hotmail.com>
2023-07-23 01:47:48 +0200

    add profile

---
+++ pages/profile.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Sidebar from "../component/sidebar";
+import styles from "@/styles/Profile.module.css";
+
+export default function Profile() {
+  const [selectedImage, setSelectedImage] = useState();
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(URL.createObjectURL(file));
+  };
+
+  return (
+    <div className={styles.container}>
+      <Sidebar />
+      <div className={styles.profile}>
+        <h2 className={styles.header}>Profile</h2>
+        <div className={styles.uploadContainer}>
+          {!selectedImage && (
+            <label className={styles.customFileUpload}>
+              <input
+                type='file'
+                accept='image/*'
+                onChange={handleImageUpload}
+                className={styles.fileInput}
+              />
+              <img src='/upload.png' alt='upload image' />
+            </label>
+          )}
+          {selectedImage && (
+            <img src={selectedImage} alt='Selected' height={200} />
+          )}
+        </div>
+        <div className={styles.inputHeader}>Company Name</div>
+        <input
+          type='text'
+          required
+          className={styles.inputSearch}
+          placeholder='Company Name'
+        />
+        <div className={styles.inputHeader}>Email</div>
+        <input
+          type='text'
+          required
+          className={styles.inputSearch}
+          placeholder='Email'
+        />
+        <button className={styles.saveButton}>Save</button>
+      </div>
+    </div>
+  );
+}
