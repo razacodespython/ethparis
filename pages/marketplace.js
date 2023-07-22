7b080e5455ca93dd4ef3b5cfb8f8133b430d0a7f
Xian Zheng <seeinplay@gmail.com>
2023-07-23 00:50:52 +0200

    marketplace stub

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../styles/Marketplace.module.css'; // Import the CSS file
+import styles from '../styles/Marketplace.module.css'; // Import the CSS module
 import Image from 'next/image';
 
 export default function Marketplace() {
@@ -17,21 +17,82 @@ export default function Marketplace() {
 
   return (
     <div>
-      <h2>Marketplace</h2>
-      <h4>Use your gift cards within this app.</h4>
-      <Image
-                src='/assets/aave.jpeg'
-                alt='Aave'
-                width={150}
-                height={150}
-                layout="responsive"
-                
-              />
-      {selectedImage && (
-        <div className="modal" onClick={handleClickAway}>
-          <img src={selectedImage} alt="Selected" />
+      <h2 className={styles.header}>Marketplace</h2>
+      <p className={styles.header}>Use your gift cards within this app.</p>
+      <div className={styles.container}>
+        <div
+          className={styles.rectangle}
+          onClick={() => handleImageClick('/assets/aave.jpeg')}
+        >
+          <Image
+            src='/assets/aave.jpeg'
+            alt='Aave'
+            width={150}
+            height={150}
+            layout="responsive"
+          />
         </div>
-      )}
+        <div
+          className={styles.rectangle}
+          onClick={() => handleImageClick('/assets/apecoin.jpeg')}
+        >
+          <Image
+            src='/assets/apecoin.jpeg'
+            alt='Aave'
+            width={150}
+            height={150}
+            layout="responsive"
+          />
+        </div>
+        <div
+          className={styles.rectangle}
+          onClick={() => handleImageClick('/assets/uniswap.png')}
+        >
+          <Image
+            src='/assets/uniswap.png'
+            alt='Aave'
+            width={150}
+            height={150}
+            layout="responsive"
+          />
+        </div>
+        <div
+          className={styles.rectangle}
+          onClick={() => handleImageClick('/assets/aave.jpeg')}
+        >
+          <Image
+            src='/assets/aave.jpeg'
+            alt='Aave'
+            width={150}
+            height={150}
+            layout="responsive"
+          />
+        </div>
+        <div
+          className={styles.rectangle}
+          onClick={() => handleImageClick('/assets/aave.jpeg')}
+        >
+          <Image
+            src='/assets/aave.jpeg'
+            alt='Aave'
+            width={150}
+            height={150}
+            layout="responsive"
+          />
+        </div>
+        <div
+          className={styles.rectangle}
+          onClick={() => handleImageClick('/assets/aave.jpeg')}
+        >
+          <Image
+            src='/assets/aave.jpeg'
+            alt='Aave'
+            width={150}
+            height={150}
+            layout="responsive"
+          />
+        </div>
+      </div>
     </div>
   );
 }
