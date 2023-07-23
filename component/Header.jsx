75c399eb4f8d5ff955b709ec50bd0f36dd7f9b28
Xian Zheng <seeinplay@gmail.com>
2023-07-23 04:48:19 +0200

    try log out

--- component/Header.jsx
+++ component/Header.jsx
@@ -1,9 +1,30 @@
 import styles from "@/styles/Profile.module.css";
+import SocialLogin from "@biconomy/web3-auth";
+import Link from "next/link";
+import { useRef } from "react";
 
 export default function Header() {
+    const sdkRef = useRef(null);
+    
+    const logout = async () => {
+        const socialLoginSDK = new SocialLogin();
+        if (!sdkRef.current) {
+            console.error("Web3Modal not initialized.");
+            return;
+        }
+        await sdkRef.current.logout();
+        sdkRef.current.hideWallet();
+        setSmartAccount(null);
+        setBiconomyAddress("");
+        enableInterval(false);
+        <Link href='/index'></Link>
+    };
   return (
     <div className={styles.heading}>
       <h1>WALA</h1>
+      <button onClick={logout} className={styles.connect}>
+          Logout
+      </button>
     </div>
   );
 }
